3650f0cab85cbc98f020b5a1dc1de1540dfe7aac
anujawankar <158019756+anujawankar@users.noreply.github.com>
2024-02-01 14:31:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cardItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cardItem.classList.remove('active');
+}
+
+let cardItem = document.querySelector('.cart-items-container');
+document.querySelector('#cart-btn').onclick = () =>{
+    cardItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cardItem.classList.remove('active');
+}
